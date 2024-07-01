0425e877e7cf9f7d0d8097b027745024c9a86e01
CreativePudding <puddingcreative@gmail.com>
2024-07-01 21:28:59 +0200

    implemented authorization behavior and logging behavior

--- Application/Common/Behaviours/AuthorizationBehaviour.cs
+++ Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -1,6 +1,68 @@
 ﻿namespace Application;
 
-public class AuthorizationBehaviour
+public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
+    private readonly IUser _user;
+    private readonly IIdentityService _identityService;
 
+    public AuthorizationBehaviour(
+        IUser user,
+        IIdentityService identityService
+        )
+    {
+        _user = user;
+        _identityService = identityService;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+        if (authorizeAttributes.Any())
+        {
+            if (_user.Id == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            var authorizeAttributesWithRoles = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Roles));
+
+            if (authorizeAttributesWithRoles.Any())
+            {
+                var authorized = false;
+                foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+                {
+                    foreach (var role in roles)
+                    {
+                        var isInRole = await _identityService.IsInRoleAsync(_user.Id, role.Trim());
+                        if (isInRole)
+                        {
+                            authorized = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!authorized)
+                {
+                    throw new ForbiddenAccessException();
+                }
+            }
+
+            var authorizeAttributesWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Policy));
+            if (authorizeAttributesWithPolicies.Any())
+            {
+                foreach (var policy in authorizeAttributesWithPolicies.Select(a => a.Policy))
+                {
+                    var authorized = await _identityService.AuthorizeAsync(_user.Id, policy);
+
+                    if (!authorized)
+                    {
+                        throw new ForbiddenAccessException();
+                    }
+                }
+            }
+        }
+        return await next();
+    }
 }
