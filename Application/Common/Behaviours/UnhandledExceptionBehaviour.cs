46a891dba12abdb9fdbb4cfb41400b8904735bf6
CreativePudding <puddingcreative@gmail.com>
2024-07-02 13:01:22 +0200

    Implemented unhandled exception behavior

--- Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
+++ Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -1,6 +1,25 @@
 ﻿namespace Application;
 
-public class UnhandledExceptionBehaviour
+public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
+    private readonly ILogger<TRequest> _logger;
+    public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
 
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TRespnse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            _logger.LogError(ex, "Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+            throw;
+        }
+    }
 }
